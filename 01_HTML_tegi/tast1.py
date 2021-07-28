3d03405fec443b74c7957f1bb65314aaa3b173d8
baranava_varvara <vary.99@mail.ru>
2021-07-28 17:43:44 +0300

    homework

---
+++ 01_HTML_tegi/tast1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+print ('Hello. I am a python program.')
+name = input('What is your name? ')
+print ('Hello there, ' + name + '!')
+
+for cntr in range(0,10):
+    print (cntr)
---
+++ 01_HTML_tegi/tast2.py
@@ -0,0 +1,6 @@
+months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+print (months[1])
+print (len(months))
+
+categories = ['Main dish','Meat','Fish','Soup','Cookies']
+print (categories[4])
